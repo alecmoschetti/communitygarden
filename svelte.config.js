6dbbef72d926251fd5d184703d51a0268465982b
alecmoschetti <alecmoschetti@gmail.com>
2021-06-10 17:37:31 -0700

    added static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,12 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: require('@sveltejs/adapter-static')(),
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
 
